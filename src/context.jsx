8c2329cf8a284cc950584a9ba8c5d83461273ad4
Mariusz Rojewski <rojewski@webkon.eu>
2023-11-15 10:06:23 +0100

    Sidebar Complated

---
+++ src/context.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return React.useContext(AppContext);
+};
